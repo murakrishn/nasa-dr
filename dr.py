b0058df345ba6d0225071667d9a3d676df607873
murakrishn <murakrishn@users.noreply.github.com>
2021-08-05 12:35:23 -0400

    d-matrix and test definitions in

--- dr.py
+++ dr.py
@@ -116,20 +116,3 @@ class DR(object):
         valid_error_modes = ["DR_ERROR_WRONG_NUM_TESTS", "DR_ERROR_INVALID_TEST_RESULT"]
 
         return check_result
-
-                
-        
-if __name__ == '__main__':
-    d_matrix = np.asarray([[1, 1, 1, 1], \
-                           [0, 1, 1, 0], \
-                           [0, 1, 1, 0], \
-                           [0, 0, 0, 1], \
-                           [0, 0, 1, 0]], dtype=np.float32)
-    dr = DR(verbose=False)
-    print("Example 1")
-    test_result1 = ["PASS", "PASS", "FAIL", "PASS"]
-    dr.reason(d_matrix, test_result1)
-
-    print("Example 2")
-    test_result2 = ["UNKNOWN", "FAIL", "FAIL", "PASS"]
-    dr.reason(d_matrix, test_result2)
---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+from dr import DR
+
+
+if __name__ == '__main__':
+    d_matrix = np.asarray([[1, 1, 1, 1], \
+                           [0, 1, 1, 0], \
+                           [0, 1, 1, 0], \
+                           [0, 0, 0, 1], \
+                           [0, 0, 1, 0]], dtype=np.float32)
+    dr = DR(verbose=False)
+    print("Example 1")
+    test_result1 = ["PASS", "PASS", "FAIL", "PASS"]
+    dr.reason(d_matrix, test_result1)
+
+    print("Example 2")
+    test_result2 = ["UNKNOWN", "FAIL", "FAIL", "PASS"]
+    dr.reason(d_matrix, test_result2)
