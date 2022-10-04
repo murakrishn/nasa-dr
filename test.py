c79ed3373ee9f1b32ff0c861dbacf0f3290271c6
murakrishn <murakrishn@users.noreply.github.com>
2022-10-04 14:42:46 -0400

    testing the implementation

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
 import numpy as np
-from dr import DR
+from pprint import pprint
+from dr._dr import DR
+from dr.dr import DiagnosticReasoner
 
 
 if __name__ == '__main__':
+    # Dependency matrix
     d_matrix = np.asarray([[1, 1, 1, 1], \
                            [0, 1, 1, 0], \
                            [0, 1, 1, 0], \
                            [0, 0, 0, 1], \
                            [0, 0, 1, 0]], dtype=np.float32)
-    dr = DR(verbose=False)
+    # Diagnostic Reasoner
+    # dr = DR(verbose=False)
+    dr = DiagnosticReasoner()
+
+    # quit()
+
+
+    # Test Example 1
     print("Example 1")
     test_result1 = ["PASS", "PASS", "FAIL", "PASS"]
     dr.process(d_matrix, test_result1)
-
+    # pprint(vars(dr))
+    # Test Example 2
     print("Example 2")
     test_result2 = ["UNKNOWN", "FAIL", "FAIL", "PASS"]
     dr.process(d_matrix, test_result2)
