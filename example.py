2d121ce8d086b850965e363cc9b95fec45945d36
murakrishn <murakrishn@users.noreply.github.com>
2022-10-07 11:47:49 -0400

    re-named and commented

--- example.py
+++ example.py
@@ -1,29 +1,33 @@
+"""Example scripts for diagnostic reasoning
+
+Author:
+    Murali Krishnan R
+    
+Date:
+    10.07.2022
+
+"""
+
 import numpy as np
-from pprint import pprint
-from dr._dr import DR
 from dr.dr import DiagnosticReasoner
 
 
 if __name__ == '__main__':
-    # Dependency matrix
+    # 1. Specify a Dependency-matrix (fault-to-effect mapping)
     d_matrix = np.asarray([[1, 1, 1, 1], \
                            [0, 1, 1, 0], \
                            [0, 1, 1, 0], \
                            [0, 0, 0, 1], \
                            [0, 0, 1, 0]], dtype=np.float32)
-    # Diagnostic Reasoner
-    # dr = DR(verbose=False)
+    # 2. Instantiate a static diagnostic reasoner
     dr = DiagnosticReasoner()
 
-    # quit()
-
-
-    # Test Example 1
+    # 3a. Example Test 1
     print("Example 1")
     test_result1 = ["PASS", "PASS", "FAIL", "PASS"]
     dr.process(d_matrix, test_result1)
-    # pprint(vars(dr))
-    # Test Example 2
+    
+    # 3b. Test Example 2
     print("Example 2")
     test_result2 = ["UNKNOWN", "FAIL", "FAIL", "PASS"]
     dr.process(d_matrix, test_result2)
